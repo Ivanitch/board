4137280357723e693c4de791e8811a80917b23fe
Sergey Ivanov <ivanitch.mail@gmail.com>
2021-04-22 21:32:16 +0900

    npm. Changed mix public path

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
+mix
+    .setPublicPath('public/build')
+    .setResourceRoot('build')
+    .js('resources/js/app.js', 'js')
+    .postCss('resources/css/app.css', 'css', [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
 ]);
